8db42411a9377475a1281605dd6d719f152bea85
vikasrajput0112 <devops1@paisalo.in>
2026-01-22 15:02:12 +0530

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => (
+  <div className="p-10 bg-white max-w-md mx-auto">
+    <h2 className="text-2xl text-pinkPrimary">User Dashboard</h2>
+    <p>Name: John Doe</p>
+    <p>Email: john@test.com</p>
+  </div>
+);
+
+export default Dashboard;
