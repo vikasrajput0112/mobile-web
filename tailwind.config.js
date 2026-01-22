4b0a5b981cd58210a0e386b25a26306285210ab5
vikasrajput0112 <devops1@paisalo.in>
2026-01-22 14:55:10 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pinkPrimary: "#e91e63",
+        pinkLight: "#fdecef",
+        pinkSoft: "#fff5f7",
+        pinkBorder: "#f8c6d0"
+      }
+    }
+  },
+  plugins: []
+};
